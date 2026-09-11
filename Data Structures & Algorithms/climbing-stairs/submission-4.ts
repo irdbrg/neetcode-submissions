be6977688815be83289440dd90f938e2ee7261b8
irdbrg <iliya.rudberg@gmail.com>
2026-09-11 14:21:29 +0300

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.ts
@@ -0,0 +1,18 @@
+class Solution {
+    climbStairs(n: number): number {
+        const memo = new Map<number, number>();
+
+        function ways(k: number): number {
+            if (k <= 2) return k;
+            if (memo.has(k)) return memo.get(k)!;
+            const result = ways(k - 1) + ways(k - 2);
+            memo.set(k, result);
+            return result;
+        }
+        
+        return ways(n);
+    }
+}
+
+// 4
+
